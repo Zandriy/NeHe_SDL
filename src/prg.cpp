83d9a2eec0f4078ed14656f65a8d839828f2c67c
Andanat_Mac <andgen@list.ru>
2013-02-20 19:35:05 +0200

    Mac compilation is fixed

--- src/prg.cpp
+++ src/prg.cpp
@@ -7,9 +7,14 @@
 
 #include <cstdio>
 
+#include <SDL/SDL.h>
 #include "NeHeConfig.h"
 
-int main()
+#if __APPLE__
+int SDL_main(int argc, char ** argv)
+#else
+int main(int argc, char ** argv)
+#endif
 {
     fprintf(stdout,"Version %d.%d\n",
     		NeHe_VERSION_MAJOR,
